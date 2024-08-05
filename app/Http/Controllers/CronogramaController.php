ae420114df82a7bbbdec9a3d1724825d0bd55327
rafael07-dev <deralato817@gmail.com>
2024-08-05 08:46:32 -0500

    return view with Inertia.js

--- app/Http/Controllers/CronogramaController.php
+++ app/Http/Controllers/CronogramaController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Cronograma;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CronogramaController extends Controller
 {
     public function index()  {
 
-        $cronogramas = Cronograma::with(['historial_mantenimientos', 'responsable'])->get();
-        return view('cronograma.index', compact('cronogramas'));
+        return Inertia::render('Cronograma/Index', [
+            'data' => Cronograma::all()
+        ]);
     }
 }
